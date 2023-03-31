3d7bccbdaaf1e3f8600cbec1c94621f27dd1131e
YUSEULBI <ha258ppy@gmail.com>
2023-03-31 17:34:08 +0900

    3-31 오후5:34

--- oimarket/src/main/java/oimarket/control/product/Product.java
+++ oimarket/src/main/java/oimarket/control/product/Product.java
@@ -2,13 +2,17 @@ package oimarket.control.product;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.fileupload.FileItem;
 import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 @WebServlet("/product")
 public class Product extends HttpServlet {
@@ -34,6 +38,17 @@ public class Product extends HttpServlet {
 		fileset.setRepository(setpath);	//파일저장소 위치 대입
 		fileset.setSizeThreshold(1024*1024*10);	//파일저장소에 저장할수 있는 최대용량 범위
 		fileset.setDefaultCharset("UTF-8"); //파일저장소 한글 인코딩 타입
+		
+		ServletFileUpload fileupload=new ServletFileUpload(fileset);
+		
+		List<FileItem> fileList=fileupload.parseRequest(request);//매개변수 요청 리스트에담기
+		
+		for(FileItem item:fileupload) {
+			
+		}
+		
+		
+		
 	}
 
 	
