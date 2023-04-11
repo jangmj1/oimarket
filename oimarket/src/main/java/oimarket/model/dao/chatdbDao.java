0a7ed8361b0e1d1db65f9ef348145e6ec9546230
504 <sksk1277@naver.com>
2023-04-11 09:48:16 +0900

    4/11 채팅하는중

--- oimarket/src/main/java/oimarket/model/dao/chatdbDao.java
+++ oimarket/src/main/java/oimarket/model/dao/chatdbDao.java
@@ -39,8 +39,7 @@ public class chatdbDao extends Dao{
 	//[장민정] 채팅출력하기
 	public ArrayList<MessageDto>getchat(int pno,int frommno,int tomno){
 		ArrayList<MessageDto>list=new ArrayList<>();
-		String sql="select * from chat where pno=? and (frommno=? or frommno=?) and (tomno=? or tomno=?);"; //지혼자 말한것도 나옴
-		//String sql="select * from chat  where pno=4 and (frommno=1 or tomno=1) and (frommno=4 or tomno=4)"; 어떤걸 써야하는지잘모르겠음
+		String sql="select * from chat where pno=? and (frommno=? and tomno=?) or(frommno=? and tomno=?)"; //지혼자 말한것도 나옴
 		try {
 			ps=con.prepareStatement(sql);
 			ps.setInt(1, pno);
